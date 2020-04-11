da7e0d5fb4aa1eac4441f326173684d492f58164
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-04-11 14:44:18 +0100

    updated to java 14

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.nick.wood.game_of_life {
     requires java.desktop;
 
     exports com.nick.wood.game_of_life.model;
+    exports com.nick.wood.game_of_life.model.IO;
     exports com.nick.wood.game_of_life.model.universe;
 }
